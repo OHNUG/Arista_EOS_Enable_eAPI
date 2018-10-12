2087ee4af350e555fc6bb7d5f359b61017c18e11
cvpadmin <cvpadmin@cvpadmin>
2018-10-12 16:03:58 -0400

    Initial Commit

--- eos_enableAPI/enableAPI.py
+++ eos_enableAPI/enableAPI.py
@@ -1,32 +1,30 @@
-import json, getpass
+import yaml, getpass
 from netmiko import ConnectHandler
-with open('../hosts.json') as hostFile:
-    hosts = json.load(hostFile)
+with open('devices.yaml') as deviceYAML:
+    devices = yaml.safe_load(deviceYAML)
 
-credUser = input("Please enter the username for ssh: ")
+credUser = raw_input("Please enter the username for ssh: ")
 credPass = getpass.getpass("Please enter the password for ssh: ")
 
 
-for group in hosts['hosts']:
-    if "eos" in group:
-        for hostname, ip in hosts['hosts'][group].items():
-            print("Establishing connection to %s..." % hostname)
+for hostname, ip in devices['devices'].items():
+    print("Establishing connection to %s..." % hostname)
 
-            #Establish Connection
-            #Try to connect to device, if there is a failure, throw the error and move on
-            try:
-                net_connect = ConnectHandler(device_type='arista_eos', ip=ip, username=credUser, password=credPass)
+    #Establish Connection
+    #Try to connect to device, if there is a failure, throw the error and move on
+    try:
+        net_connect = ConnectHandler(device_type='arista_eos', ip=ip, username=credUser, password=credPass)
 
-                # Enter privileged exec, then Global Configuration
-                net_connect.enable()
-                config = net_connect.config_mode()
+        # Enter privileged exec, then Global Configuration
+        net_connect.enable()
+        config = net_connect.config_mode()
 
-                # Send Commands to enable eAPIs
-                print("Enabling eAPI...")
-                net_connect.send_config_set(["management api http-commands", "no shutdown", "write"])
+        # Send Commands to enable eAPIs
+        print("Enabling eAPI...")
+        net_connect.send_config_set(["management api http-commands", "no shutdown", "write"])
 
-                print("eAPI enabled on %s." % hostname)
+        print("eAPI enabled on %s." % hostname)
 
-            except:
-                print("*****Error connecting to %s*****" % hostname)
+    except:
+        print("*****Error connecting to %s*****" % hostname)
 
